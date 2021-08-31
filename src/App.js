cd15d19b7abd909c2903999ceb27d0591d9bbe5f
Thomas Sankara <thomassankara74@gmail.com>
2021-08-31 10:20:24 +0300

    style h1, refactor component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css"
-import FetchQuestions from "./components/FetchQuestions"
+import SetupForm from "./components/SetupForm"
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Trivia App</h1>
-      <FetchQuestions />
+      <h1 className="font-bold uppercase tracking-wide text-4xl lg:text-8xl mt-5">
+        Trivia App
+      </h1>
+      <SetupForm />
     </div>
   )
 }
