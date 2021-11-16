4884d5fa3ff601b44ecd7bdff6b85f811ef0b7fb
Thomas Sankara <thomassankara74@gmail.com>
2021-11-16 10:00:09 +0300

    Show Questions component

--- src/components/ShowQuestions.js
+++ src/components/ShowQuestions.js
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 export default function ShowQuestions({ questions }) {
   const [alert, setAlert] = useState({
     msg: "",
@@ -12,26 +14,31 @@ export default function ShowQuestions({ questions }) {
     <>
       <section>
         <div className="text-left mx-10 sm:w-3/4 sm:mx-auto lg:w-1/2 mt-10 grid grid-cols-1 gap-10 pb-20">
-          {questions.map(({ correct_answer, incorrect_answers, question }) => (
-            <article key={question}>
-              <h3 className="font-bold text-xl">{question}</h3>
-              <ul>
-                {incorrect_answers.map((ans) => (
-                  <li>
-                    <button className="bg-red-500 py-2 px-4 rounded my-2 text-white cursor-pointer w-full text-left select-none">
-                      {ans}
-                    </button>
-                  </li>
-                ))}
-              </ul>
-              <p
-                className="bg-green-500 py-2 px-4 rounded my-2 text-white cursor-pointer select-none"
-                onClick={handleCorrect}
-              >
-                {correct_answer}
-              </p>
-            </article>
-          ))}
+          {questions.map(
+            ({ correct_answer, incorrect_answers, question, index }) => (
+              <article key={index}>
+                <h3
+                  className="font-bold text-xl"
+                  dangerouslySetInnerHTML={{ __html: question }}
+                ></h3>
+                <ul>
+                  {incorrect_answers.map((ans) => (
+                    <li key={ans}>
+                      <button
+                        dangerouslySetInnerHTML={{ __html: ans }}
+                        className="bg-red-500 py-2 px-4 rounded my-2 text-white cursor-pointer w-full text-left select-none"
+                      ></button>
+                    </li>
+                  ))}
+                </ul>
+                <p
+                  className="bg-green-500 py-2 px-4 rounded my-2 text-white cursor-pointer select-none"
+                  onClick={handleCorrect}
+                  dangerouslySetInnerHTML={{ __html: correct_answer }}
+                ></p>
+              </article>
+            )
+          )}
         </div>
       </section>
     </>
